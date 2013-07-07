b67317be8e7a5f5c9537aa61e4ce5d1d991eab37
David Demerson <ddemerso@andrew.cmu.edu>
2013-07-07 13:46:41 -0400

    New behavior controls added student behaviors modal

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.controller('MenuStudentListCtrl', function($scope, students, viewService) {
 
 // maintains view state data for main content area for the student view
 app.controller('MainStudentCtrl', function($scope, students, viewService) {
-    $scope.settingsActive = false
+    $scope.behaviorModalActive = false
 
     $scope.$watch(function() {return viewService}, function(data) {
         $scope.student = students.students[data.parameters._id];
@@ -51,11 +51,7 @@ app.controller('MainStudentCtrl', function($scope, students, viewService) {
     }, true);
 
     $scope.showSettings = function() {
-        $scope.settingsActive = true;
-    }
-
-    $scope.hideSettings = function() {
-        $scope.settingsActive = false;
+        $scope.behaviorModalActive = true;
     }
 });
 
@@ -113,6 +109,25 @@ app.directive('mainTabs', function() {
     };
 });
 
+app.controller('behaviorModalCtrl', function($scope) {
+    $scope.addingBehavior = false;
+    $scope.behaviorTypes = ['Frequency', 'Duration'];
+    $scope.selectedBehaviorType = null;
+
+    $scope.showAddBehavior = function() {
+        $scope.addingBehavior = true;
+    }
+
+    $scope.hideAddBehavior = function() {
+        $scope.addingBehavior = false;
+    }
+
+    $scope.saveBehavior = function() {
+        console.log($scope.description, $scope.selectedBehaviorType);
+        $scope.addingBehavior = false;
+    }
+});
+
 app.directive('mainPane', function() {
     return {
         require: '^mainTabs',
@@ -149,6 +164,34 @@ app.directive('mainModal', function() {
             '</div>',
         replace: true
     }
+})
+
+app.directive('buttonset', function() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        scope: {options: '=', value: '=', label: '@'},
+        controller: function($scope, $element, $attrs) {
+            //$scope.selectedIndex = $scope.options.indexOf($scope.value);
+            $scope.select = function(index) {
+                $scope.selectedIndex = index;
+                $scope.value = $scope.options[index];
+
+            }
+            $scope.isSelected = function(index) {
+                return index === $scope.selectedIndex;
+            }
+
+        },
+        template:
+            '<div>' +
+                '<h3>{{label}}</h3>' +
+                '<ul class="buttonset">' +
+                    '<li ng-repeat="option in options" ng-click="select($index)" ng-class="{selected: isSelected($index)}">{{option}}</li>' +
+                '</ul>' +
+            '</div>',
+        replace: true
+    };
 })
 
 
