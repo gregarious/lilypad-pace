0198409ac346bedd293f19a2de46d47926a23908
David Demerson <ddemerso@andrew.cmu.edu>
2013-07-08 09:51:50 -0400

    Added old counter widget (needs work)

--- modules/widgets.js
+++ modules/widgets.js
@@ -1,4 +1,4 @@
-var widgets = angular.module('widgets', []);
+var widgets = angular.module('widgets', ['underscore']);
 
 widgets.directive('buttonset', function() {
     return {
@@ -26,4 +26,58 @@ widgets.directive('buttonset', function() {
                 '</div>',
         replace: true
     };
-})
+});
+
+widgets.directive('counter', function() {
+    return {
+        restrict: 'E',
+        scope: { max: '@', min: '@', text: '@', value: '='},
+        controller: function($scope, $element, $attrs) {
+
+            // sanity checks
+            if ($attrs.max === undefined || $attrs.min === undefined || $scope.value === undefined) {
+                throw "Max, min, and value must all be defined";
+                if ($attrs.max <= $attrs.min ) {
+                    throw "Max must be greater than min for counter";
+                }
+            }
+
+            // array of possible values for counter values
+            $scope.values = _.range(parseInt($attrs.min), parseInt($attrs.max) + 1).reverse();
+
+            // updates the counter's margin to move visible number
+            var updateCounter = function() {
+                var counterElement = $element[0].children[1].children[0]
+                counterElement.setAttribute("style", "margin-top:" + (- ($scope.max - $scope.value) * counterElement.clientHeight).toString() + "px")
+            }
+
+            $scope.increment = function() {
+                $scope.value = Math.min($scope.max, $scope.value + 1);
+                updateCounter();
+            }
+
+            $scope.decrement = function() {
+                $scope.value = Math.max($scope.min, $scope.value - 1);
+                updateCounter();
+            }
+
+            // set the counter to the correct initial value when element is ready
+            $element.ready(function() {
+                updateCounter();
+                // using setTimeout to avoid animating counters too early
+                setTimeout(function() {
+                    $element[0].children[1].children[0].className = $element[0].children[1].children[0].className + " animated";
+                }, 0);
+            })
+        },
+        template: '<div class="frequency-counter">' +
+            '<div class="arrow-container" ng-click="increment()"><div class="arrow-up"></div></div>' +
+            '<div class="countWrapper">' +
+            '<div class="count" ng-repeat="i in values">{{i}}</div>' +
+            '</div>' +
+            '<div class="name">{{text}}</div>' +
+            '<div class="arrow-container" ng-click="decrement()"><div class="arrow-down"></div></div>' +
+            '</div>',
+        replace: true
+    };
+});
