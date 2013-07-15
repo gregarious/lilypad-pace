35317cfac5a4fd0b00a35c04bb4d88549db08f5c
Greg Nicholas <greg.nicholas@gmail.com>
2013-07-15 13:00:01 -0400

    add case transformations to API response/request handling

--- modules/modules.js
+++ modules/modules.js
@@ -8,7 +8,49 @@ underscore.factory('_', function() {
     return window._; // assumes underscore has already been loaded on the page
 });
 
-// backbone relational not implemented yet
+// some setup work on Backbone to allow for ease transition from API server variable
+// letter case conventions during serialization
+(function(){
+    // stolen from Ember.js
+    var STRING_CAMELIZE_REGEXP = (/(\-|_|\.|\s)+(.)?/g);
+    var camelize = function(str) {
+        return str.replace(STRING_CAMELIZE_REGEXP, function(match, separator, chr) {
+            return chr ? chr.toUpperCase() : '';
+        })
+        .replace(/^([A-Z])/, function(match, separator, chr) {
+            return match.toLowerCase();
+        });
+    };
+    var STRING_DECAMELIZE_REGEXP = (/([a-z])([A-Z])/g);
+    var decamelize = function(str) {
+        return str.replace(STRING_DECAMELIZE_REGEXP, '$1_$2').toLowerCase();
+    };
+
+    // Monkey-patch parse and toJSON to handle case conversions between client
+    // and server case conventions (snakecase on server, camelcase on client)
+    var originalToJSON = Backbone.Model.prototype.toJSON;
+    Backbone.Model = Backbone.Model.extend({
+        parse: function(response, options) {
+            var camelResponse = {};
+            // TODO: use proper modularized version of underscore
+            _.each(_.pairs(response), function(kv_pair) {
+                camelResponse[camelize(kv_pair[0])] = kv_pair[1];
+            });
+            return camelResponse;
+        },
+        toJSON: function() {
+            var data = originalToJSON.apply(this, arguments);
+            var snakeData = {};
+            // TODO: use proper modularized version of underscore
+            _.each(_.pairs(data), function(kv_pair) {
+                snakeData[decamelize(kv_pair[0])] = kv_pair[1];
+            });
+            return snakeData;
+        }
+    });
+
+
+})();
 
 var backbone = angular.module('backbone', ['underscore']);
 backbone.factory('Backbone', function() {
@@ -16,5 +58,5 @@ backbone.factory('Backbone', function() {
 });
 
 angular.module('moment', []).factory('moment', function() {
-	return window.moment;
+    return window.moment;
 });
