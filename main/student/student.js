289e610856c1e6898bd7ea84010bf5277be4b419
Greg Nicholas <greg.nicholas@gmail.com>
2013-08-16 13:01:34 -0400

    convert to use accessor promise paradigm

--- main/student/student.js
+++ main/student/student.js
@@ -1,9 +1,15 @@
 // parent controller to all student main views (collect, analyze, discuss)
 app.controller('MainStudentCtrl', function ($scope, studentAccessors, viewService) {
-    var students = studentAccessors.allStudents();
-    $scope.$watch(function () {
-        return viewService
-    }, function (data) {
-        $scope.student = students.get([data.parameters.id]);
-    }, true);
+    $scope.student = null;
+
+	var fetchStudent = studentAccessors.allStudents();
+	var students;
+	fetchStudent.then(function(collection) {
+		students = collection;
+		$scope.$watch(function () {
+			return viewService;
+		}, function (data) {
+			$scope.student = students.get(data.parameters.id);
+		}, true);
+    });
 });
