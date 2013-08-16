710f5fe4e4de811fb7f16802747fedff3846d9be
Greg Nicholas <greg.nicholas@gmail.com>
2013-08-16 14:06:41 -0400

    convert behavior accessors client code to use promises

--- main/student/collect/incidentLog.js
+++ main/student/collect/incidentLog.js
@@ -1,47 +1,60 @@
 // controller for the incident log
-app.controller('MainStudentCollectIncidentLogCtrl', function ($scope, studentAccessors, behaviorIncidentAccessors, viewService) {
-    var students = studentAccessors.allStudents();
+app.controller('MainStudentCollectIncidentLogCtrl', function ($scope, studentAccessors, behaviorIncidentAccessors, viewService, $q) {
     $scope.period = {};
     $scope.data = {};
     $scope.addingIncident = false;
-    $scope.$watch(function () {
-        return viewService
-    }, function (data) {
-        $scope.student = students.get([data.parameters.id]);
-        $scope.incidentCollection = behaviorIncidentAccessors.dailyStudentIncidents($scope.student);
-        $scope.incidents = $scope.incidentCollection.models;
-        $scope.incidentTypes = behaviorIncidentAccessors.studentBehaviorTypes($scope.student).models;
-    }, true);
+    $scope.incidentTypes = []
+    $scope.incidentCollection = null;
+    $scope.incidents = [];
 
-    // opens the "new incident" control
-    $scope.openNewIncident = function () {
-        $scope.addingIncident = true;
-    };
+    var fetchStudent = studentAccessors.allStudents();
+    fetchStudent.then(function(students) {
+        $scope.$watch(function () {
+            return viewService;
+        }, function (data) {
+            $scope.student = students.get(data.parameters.id);
 
-    // closes and clears the "new incident" control
-    $scope.closeNewIncident = function () {
-        $scope.addingIncident = false;
-        $scope.data.type = $scope.data.startedAt = $scope.data.endedAt = $scope.data.comment = null;
-    };
+            var fetchTypes = behaviorIncidentAccessors.studentBehaviorTypes($scope.student).models;
+            var fetchIncidents = behaviorIncidentAccessors.dailyStudentIncidents($scope.student);
+            var fetchAll = $q.all(fetchTypes, fetchIncidents);
+            fetchAll.then(function(collections) {
+                $scope.incidentTypes = collections[0].models;
+                $scope.incidentCollection = collections[1];
+                $scope.incidents = $scope.incidentCollection.models;
+            });
 
-    // TODO: Should be doing responsive form validation here
-    // TODO: Should confirm new incidents for students marked absent
-    $scope.submitIncident = function () {
-        var today = new Date();
-        $scope.data.startedAt.split(':');
-        today.setHours($scope.data.startedAt[0]);
-        today.setMinutes($scope.data.startedAt[1]);
-        $scope.data.startedAt = today;
-        if ($scope.data.endedAt) {
-            today.setHours($scope.data.endedAt[0]);
-            today.setMinutes($scope.data.endedAt[1]);
+        }, true);
+
+        // opens the "new incident" control
+        $scope.openNewIncident = function () {
+            $scope.addingIncident = true;
+        };
+
+        // closes and clears the "new incident" control
+        $scope.closeNewIncident = function () {
+            $scope.addingIncident = false;
+            $scope.data.type = $scope.data.startedAt = $scope.data.endedAt = $scope.data.comment = null;
+        };
+
+        // TODO: Should be doing responsive form validation here
+        // TODO: Should confirm new incidents for students marked absent
+        $scope.submitIncident = function () {
+            var today = new Date();
+            $scope.data.startedAt.split(':');
+            today.setHours($scope.data.startedAt[0]);
+            today.setMinutes($scope.data.startedAt[1]);
             $scope.data.startedAt = today;
-        }
-        $scope.incidentCollection.createIncident(
-            $scope.data.type,
-            $scope.data.startedAt,
-            $scope.data.endedAt,
-            $scope.data.comment);
-        $scope.closeNewIncident();
-    };
+            if ($scope.data.endedAt) {
+                today.setHours($scope.data.endedAt[0]);
+                today.setMinutes($scope.data.endedAt[1]);
+                $scope.data.startedAt = today;
+            }
+            $scope.incidentCollection.createIncident(
+                $scope.data.type,
+                $scope.data.startedAt,
+                $scope.data.endedAt,
+                $scope.data.comment);
+            $scope.closeNewIncident();
+        };
+    });
 });
