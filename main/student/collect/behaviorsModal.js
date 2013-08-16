fd240cc4361de1b219e2055e42a6224c75223030
Greg Nicholas <greg.nicholas@gmail.com>
2013-08-16 14:09:46 -0400

    convert behaviors modal to use promises

--- main/student/collect/behaviorsModal.js
+++ main/student/collect/behaviorsModal.js
@@ -5,30 +5,34 @@ app.controller('MainStudentCollectBehaviorsModalCtrl', function ($scope, student
     $scope.behaviorTypes = ['Frequency', 'Duration'];
     $scope.selectedBehaviorType = null;
     $scope.data = {};
-    $scope.$watch(function () {
-        return viewService
-    }, function (data) {
-        $scope.student = students.get([data.parameters.id]);
-        $scope.studentTypes = _.filter($scope.incidentTypes, function (type) {
-            return type.get('applicableStudent') !== null
-        });
-    }, true);
 
-    // open the "add custom behavior" control
-    $scope.openNewBehavior = function () {
-        $scope.addingBehavior = true;
-    };
+    var fetchStudent = studentAccessors.allStudents();
+    fetchStudent.then(function(students) {
+        $scope.$watch(function () {
+            return viewService
+        }, function (data) {
+            $scope.student = students.get([data.parameters.id]);
+            $scope.studentTypes = _.filter($scope.incidentTypes, function (type) {
+                return type.get('applicableStudent') !== null
+            });
+        }, true);
 
-    // close and clear the "add custom behavior" control
-    $scope.closeNewBehavior = function () {
-        $scope.addingBehavior = false;
-        $scope.label = $scope.data.selectedBehaviorType = null;
-    };
+        // open the "add custom behavior" control
+        $scope.openNewBehavior = function () {
+            $scope.addingBehavior = true;
+        };
 
-    // submit a new behavior
-    // TODO: Should be doing responsive form validation here
-    $scope.submitNewBehavior = function () {
-        behaviorIncidentAccessors.studentBehaviorTypes($scope.student).createIncidentType($scope.label, $scope.data.selectedBehaviorType === 'Duration', null);
-        $scope.closeNewBehavior();
-    };
+        // close and clear the "add custom behavior" control
+        $scope.closeNewBehavior = function () {
+            $scope.addingBehavior = false;
+            $scope.label = $scope.data.selectedBehaviorType = null;
+        };
+
+        // submit a new behavior
+        // TODO: Should be doing responsive form validation here
+        $scope.submitNewBehavior = function () {
+            behaviorIncidentAccessors.studentBehaviorTypes($scope.student).createIncidentType($scope.label, $scope.data.selectedBehaviorType === 'Duration', null);
+            $scope.closeNewBehavior();
+        };
+    });
 });
