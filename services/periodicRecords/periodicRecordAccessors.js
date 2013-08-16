aa432f3964b9ba3e1645fae1278e4a0da1fddaa1
Greg Nicholas <greg.nicholas@gmail.com>
2013-08-16 11:58:50 -0400

    convert accessor to return promise for Collection

--- services/periodicRecords/periodicRecordAccessors.js
+++ services/periodicRecords/periodicRecordAccessors.js
@@ -1,16 +1,16 @@
-angular.module('pace').factory('periodicRecordAccessors', function(Backbone, moment, Student, DailyStudentRecordCollection) {
+angular.module('pace').factory('periodicRecordAccessors', function(Backbone, moment, Student, DailyStudentRecordCollection, $q) {
     // DailyStudentRecordCollection store
     var dailyRecordsStore = {};
     /**
-     * Returns a DailyStudentRecordCollection with records for the 
+     * Returns a DailyStudentRecordCollection with records for the
      * given student and date.
-     * 
+     *
      * @param  {Student} student
      * @param  {String} dateString    default: today's date as ISO string
      * @param  {String} options       if {refresh: true} in options, a fetch
      *                                will be performed when the collection
      *                                already exists.
-     * 
+     *
      * @return {DailyStudentRecordCollection}
      */
     var dailyStudentRecords = function(student, dateString, options) {
@@ -29,12 +29,25 @@ angular.module('pace').factory('periodicRecordAccessors', function(Backbone, mom
             refresh = true;
             dailyRecordsStore[cacheKey] = recordCollection;
         }
+
+        var deferred = $q.defer();
         if (refresh) {
             // TODO: revisit the non-destructive nature of the fetch
-            recordCollection.fetch({remove: false});
+            recordCollection.fetch({
+                remove: false,
+                success: function(collection, resp, options) {
+                    deferred.resolve(collection);
+                },
+                error: function(collection, resp, options) {
+                    deferred.resolve(resp);
+                }
+            });
+        }
+        else {
+            deferred.resolve(recordCollection);
         }
 
-        return recordCollection;
+        return deferred.promise;
     };
 
     /** Public interface of service **/
