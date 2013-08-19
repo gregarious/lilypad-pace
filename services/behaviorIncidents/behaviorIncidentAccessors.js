09daa0f64866d5777ced5843d760ef415706f3f3
Greg Nicholas <greg.nicholas@gmail.com>
2013-08-19 15:12:37 -0400

    remove DailyStudentIncidentCollection code

--- services/behaviorIncidents/behaviorIncidentAccessors.js
+++ services/behaviorIncidents/behaviorIncidentAccessors.js
@@ -1,10 +1,7 @@
-angular.module('pace').factory('behaviorIncidentAccessors', function(_, Backbone, moment, Student, StudentBehaviorTypeCollection, DailyStudentIncidentCollection, BehaviorIncident, $q) {
+angular.module('pace').factory('behaviorIncidentAccessors', function(_, Backbone, moment, Student, StudentBehaviorTypeCollection, BehaviorIncident, $q) {
 
     // internal StudentBehaviorTypeCollection cache
     var behaviorTypesStore = {};
-
-    // internal DailyStudentIncidentCollection cache
-    var dailyIncidentsStore = {};
 
     var exports = {};
     /**
@@ -78,58 +75,8 @@ angular.module('pace').factory('behaviorIncidentAccessors', function(_, Backbone
         return newIncident;
     };
 
-    /**
-     * Returns a DailyStudentIncidentCollection with incidents for the
-     * given student and date.
-     *
-     * @param  {Student} student
-     * @param  {String} dateString    default: today's date as ISO string
-     * @param  {String} options       if {refresh: true} in options, a fetch
-     *                                will be performed when the collection
-     *                                already exists.
-     * @return {DailyStudentIncidentCollection}
-     */
-
-    var dailyStudentIncidents = function(student, dateString, options) {
-        // use today's day if no date was provided
-        dateString = dateString || moment().format('YYYY-MM-DD');
-        var refresh = options && options.refresh;
-
-        var cacheKey = student.id + '/' + dateString;
-
-        var incidentCollection = dailyIncidentsStore[cacheKey];
-        if (!incidentCollection) {
-            incidentCollection = new DailyStudentIncidentCollection([], {
-                student: student,
-                dateString: dateString
-            });
-            refresh = true;
-            dailyIncidentsStore[cacheKey] = incidentCollection;
-        }
-
-        var deferred = $q.defer();
-        if (refresh) {
-            // TODO: revisit the non-destructive nature of the fetch
-            incidentCollection.fetch({
-                remove: false,
-                success: function(collection, resp, options) {
-                    deferred.resolve(collection);
-                },
-                error: function(collection, resp, options) {
-                    deferred.resolve(resp);
-                }
-            });
-        }
-        else {
-            deferred.resolve(incidentCollection);
-        }
-
-        return deferred.promise;
-    };
-
     return {
         studentBehaviorTypes: studentBehaviorTypes,
-        dailyStudentIncidents: dailyStudentIncidents,
         createIncident: createIncident
     };
 });
--- tests/services/behaviorIncidents/behaviorIncidentAccessorsTests.js
+++ tests/services/behaviorIncidents/behaviorIncidentAccessorsTests.js
@@ -63,35 +63,4 @@ describe("behaviorIncidentAccessors", function() {
             expect(newIncident.get('comment')).toBe("");
         });
     });
-
-    xdescribe(".dailyStudentIncidents", function() {
-        var student, dateString, dailyStudentIncidents, incidents;
-        beforeEach(inject(function(Student, behaviorIncidentAccessors) {
-            dailyStudentIncidents = behaviorIncidentAccessors.dailyStudentIncidents;
-
-            // mock out syncing for now
-            spyOn(Backbone, 'sync');
-
-            student = new Student({id: 4});
-            dateString = '2013-01-01';
-            incidents = dailyStudentIncidents(student, dateString);
-        }));
-
-        it('returns a DailyStudentIncidentCollection with expected settings', inject(function(DailyStudentIncidentCollection) {
-            expect(incidents.constructor).toBe(DailyStudentIncidentCollection);
-            expect(incidents._student).toBe(student);
-            expect(incidents._dateString).toBe('2013-01-01');
-        }));
-
-        it('returns the same collection instance when called twice', function() {
-            var incidents2 = dailyStudentIncidents(student, dateString);
-            expect(incidents).toBe(incidents2);
-        });
-
-        it("returns collection for today's date if none is specified", function() {
-            var today = moment().format('YYYY-MM-DD');
-            var todayIncidents = dailyStudentIncidents(student);
-            expect(todayIncidents._dateString).toBe(today);
-        });
-    });
 });
