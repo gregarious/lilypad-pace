b7709eb69083c8b0958604aa1b96cd9e2b1850bd
Greg Nicholas <greg.nicholas@gmail.com>
2013-08-20 17:08:34 -0400

    add sync logic

--- services/common/apiBackedCollection.js
+++ services/common/apiBackedCollection.js
@@ -1,11 +1,24 @@
 angular.module('pace').factory('APIBackedCollection', function(Backbone) {
     return Backbone.Collection.extend({
+        _isSyncInProgress: false,
+        _lastSyncedAt: null,
+
         isSyncInProgress: function() {
-            return false;
+            return this._isSyncInProgress;
         },
 
         lastSyncedAt: function() {
-            return null;
+            return this._lastSyncedAt;
+        },
+
+        sync: function(method, collection, options) {
+            var httpPromise = Backbone.Collection.prototype.sync.apply(this, arguments);
+            this._isSyncInProgress = true;
+            httpPromise.always(function() {
+                this._isSyncInProgress = false;
+                this._lastSyncedAt = new Date();
+            });
+            return httpPromise;
         }
     });
 });
--- tests/services/common/apiBackedCollectionTests.js
+++ tests/services/common/apiBackedCollectionTests.js
@@ -2,16 +2,46 @@ describe("APIBackedCollection", function() {
     describe("instance", function() {
         var collection;
         beforeEach(inject(function(APIBackedCollection) {
-            collection = new APIBackedCollection();
+            collection = new (APIBackedCollection.extend({
+                url: '/'
+            }))();
         }));
 
-        // TODO: add specs that actually show behavior of these functions, after $http fix
-        it('.isSyncInProgress', inject(function(_) {
-            expect(_.isFunction(collection.isSyncInProgress)).toBe(true);
-        }));
+        describe('initial settings', function() {
+            it('.isSyncInProgress should be false', function() {
+                expect(collection.isSyncInProgress()).toBe(false);
+            });
+            it('.lastSyncedAt should be null', function() {
+                expect(collection.lastSyncedAt()).toBeNull();
+            });
+        });
 
-        it('.lastSyncedAt', inject(function(_) {
-            expect(_.isFunction(collection.lastSyncedAt)).toBe(true);
-        }));
+        describe('immediately after fetch is called', function() {
+            beforeEach(function() {
+                collection.fetch();
+            });
+
+            it('.isSyncInProgress should be true', function() {
+                expect(collection.isSyncInProgress()).toBe(true);
+            });
+            it('.lastSyncedAt should be null', function() {
+                expect(collection.lastSyncedAt()).toBeNull();
+            });
+        });
+
+        // TODO: after $http mocking works
+        xdescribe('after fetch returns', function() {
+            beforeEach(function() {
+                collection.fetch();
+                // TODO: http flush
+            });
+
+            it('.isSyncInProgress should be false', function() {
+                expect(collection.isSyncInProgress()).toBe(false);
+            });
+            it('.lastSyncedAt should be set', function() {
+                expect(collection.lastSyncedAt()).not.toBeNull();
+            });
+        });
     });
 });
