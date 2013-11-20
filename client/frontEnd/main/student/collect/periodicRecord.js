bd50272e80ca61e08225aa1dd619ab3d50fcdf03
Greg Nicholas <greg.nicholas@gmail.com>
2013-11-19 23:10:24 -0500

    add manual period changing via "Point Review Complete" button

--- client/frontEnd/main/student/collect/periodicRecord.js
+++ client/frontEnd/main/student/collect/periodicRecord.js
@@ -6,15 +6,13 @@ app.controller('MainStudentCollectPeriodicRecordCtrl', function ($scope, periodi
     // We are inheriting $scope.collectData from parent controller.
     // #refactor
 
-    // Initialize $scope.data.selectedPeriodNumber to be the current period
-    // according to the timeTracker
-    $scope.data = {
-        selectedPeriodNumber: timeTracker.currentPeriodNumber
+    // Initialize variable used to control selected period
+    $scope.periodSelector = {
+        availablePeriods: [],
+        selectedPeriodNumber: timeTracker.currentPeriodNumber,
+        maxPeriodValue: null,
     };
-
-    // Initialize $scope.availablePeriods/maxPeriodValue
-    $scope.availablePeriods = calculateAvailablePeriods(timeTracker.currentPeriodNumber);
-    $scope.maxPeriodValue = _.max(_.pluck($scope.availablePeriods, 'value'));
+    configurePeriodSelector();
 
     /** actions on $scope **/
     // decrement points
@@ -25,55 +23,75 @@ app.controller('MainStudentCollectPeriodicRecordCtrl', function ($scope, periodi
     /** Watches on $scope **/
 
     // watch the select input for changes
-    $scope.$watch('data.selectedPeriodNumber', function(val) {
+    $scope.$watch('periodSelector.selectedPeriodNumber', function(val) {
         showPeriod(val);
+    });
+
+    // if the current period changes, it was either user-initiated or due to
+    // an out-of-date internal current period getting back into sync. Either way
+    // we should increment the selector
+    $scope.$watch('timeTracker.currentPeriodNumber', function(newPeriodNumber) {
+        $scope.periodSelector.selectedPeriodNumber = newPeriodNumber;
     });
 
     // when the recordCollection changes, need to update $scope.selectedPeriod
     $scope.$watch('collectData.periodicRecordCollection', function(recordCollection) {
-        showPeriod($scope.data.selectedPeriodNumber);
+        showPeriod($scope.periodSelector.selectedPeriodNumber);
     });
 
     // when the the length of the records change, update the dropdown of available periods
     $scope.$watch('collectData.periodicRecordCollection.length', function(recordCollection) {
-        $scope.availablePeriods = calculateAvailablePeriods(timeTracker.currentPeriodNumber);
-        $scope.maxPeriodValue = _.max(_.pluck($scope.availablePeriods, 'value'));
-    });
-
-    // if the current period changes, and we're currently displaying the current period,
-    // we should auto-increment to the next one
-    $scope.$watch('timeTracker.currentPeriodNumber', function(newPeriodNumber) {
-        if ($scope.data.selectedPeriodNumber === newPeriodNumber-1) {
-            $scope.data.selectedPeriodNumber = newPeriodNumber;
-        }
+        configurePeriodSelector();
     });
 
     // Advances to next period
     $scope.prevPeriod = function() {
-        if ($scope.data.selectedPeriodNumber > 1) {
-            $scope.data.selectedPeriodNumber = $scope.data.selectedPeriodNumber - 1;
+        if ($scope.periodSelector.selectedPeriodNumber > 1) {
+            $scope.periodSelector.selectedPeriodNumber = $scope.periodSelector.selectedPeriodNumber - 1;
         }
     };
 
     // Moves to previous period
     $scope.nextPeriod = function() {
-        if ($scope.data.selectedPeriodNumber + 1 <= $scope.maxPeriodValue) {
-            $scope.data.selectedPeriodNumber = $scope.data.selectedPeriodNumber + 1;
+        if ($scope.periodSelector.selectedPeriodNumber + 1 <= $scope.periodSelector.maxPeriodValue) {
+            $scope.periodSelector.selectedPeriodNumber = $scope.periodSelector.selectedPeriodNumber + 1;
         }
     };
 
+    $scope.finishPointReview = function () {
+        timeTracker.progressToNextPeriod();
+    };
+
+    $scope.enableMoveToNewPeriod = function() {
+        // only enable "Point Review Complete" button if we're looking
+        // at the current period and we're not at the end of the day
+        return $scope.periodSelector.selectedPeriodNumber === $scope.periodSelector.maxPeriodValue &&
+                $scope.periodSelector.selectedPeriodNumber < timeTracker.finalPeriodNumber;
+    };
+
+
     /** Implementation details **/
-    function calculateAvailablePeriods(maxPeriod) {
-        if (maxPeriod) {
-            return _.map(_.range(1, maxPeriod+1), function(pd) {
-                return {
-                    label: 'Period ' + pd,
-                    value: pd
-                };
-            });
-        }
-        else {
-            return [];
+
+    /**
+     * Set $scope.periodSelector values based on current collectData available.
+     */
+    function configurePeriodSelector() {
+        // default values in case no data is available
+        $scope.periodSelector.availablePeriods = [];
+        $scope.periodSelector.maxPeriodValue = null;
+
+        // if collect data is available
+        if ($scope.collectData && $scope.collectData.periodicRecordCollection) {
+            var periodNumbers = $scope.collectData.periodicRecordCollection.getAvailablePeriods();
+            if (periodNumbers.length > 0) {
+                $scope.periodSelector.availablePeriods = _.map(periodNumbers, function(pdNum) {
+                    return {
+                        label: 'Period ' + pdNum,
+                        value: pdNum
+                    };
+                });
+                $scope.periodSelector.maxPeriodValue = _.max(periodNumbers);
+            }
         }
     }
 
