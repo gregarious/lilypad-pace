4824c6b459f0cf56c31e72e1bd50a1846e83be73
Greg Nicholas <greg.nicholas@gmail.com>
2013-11-20 23:12:48 -0500

    fix frontEnd url typos

--- client/frontEnd/main/student/analyze.js
+++ client/frontEnd/main/student/analyze.js
@@ -1,9 +1,9 @@
 // parent controller for analyze
 app.controller('MainStudentAnalyzeCtrl', function ($scope) {
     $scope.views = [
-        {name: 'Behavior Log', url: 'frontend/main/student/analyze/behaviorLog.html'},
-        {name: 'Attendance Records', url: 'frontend/main/student/analyze/attendanceLog.html'}
+        {name: 'Behavior Log', url: 'frontEnd/main/student/analyze/behaviorLog.html'},
+        {name: 'Attendance Records', url: 'frontEnd/main/student/analyze/attendanceLog.html'}
     ];
 
-    // Temorarily removed: {name: 'Treatment Period', url: 'frontend/main/student/analyze/treatmentPeriod.html'},
+    // Temorarily removed: {name: 'Treatment Period', url: 'frontEnd/main/student/analyze/treatmentPeriod.html'},
 });
