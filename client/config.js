d885ab1b73d70a86f9fb82a194e6410ba5844a33
Greg Nicholas <greg.nicholas@gmail.com>
2013-11-20 23:17:41 -0500

    remove timetracker anchor fixing

--- client/config.js
+++ client/config.js
@@ -6,10 +6,3 @@ angular.module('mixpanel').config(function(mixpanelProvider) {
     // (only works if not real Mixpanel is not activated)
     mixpanelProvider.logStubCalls();
 });
-
-angular.module('pace').config(function(timeTrackerProvider) {
-    // override timeTracker for development by setting app time to set app
-    // time to 8/20/2013 at 10am
-    timeTrackerProvider.setAnchorTime(new Date(2013, 8, 5, 10, 10));
-});
-
