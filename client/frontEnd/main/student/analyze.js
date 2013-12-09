0417a8df93ded3e4d40e5585bbcbcb8c990752de
Kevin Schaefer <kevinjschaefer@gmail.com>
2013-12-09 03:08:37 -0500

    Added treatment report w/ fake data

--- client/frontEnd/main/student/analyze.js
+++ client/frontEnd/main/student/analyze.js
@@ -2,9 +2,8 @@
 app.controller('MainStudentAnalyzeCtrl', function ($scope) {
     $scope.views = [
         {name: 'Rules', url: 'frontEnd/main/student/analyze/rules.html'},
+        {name: 'Treatment Report', url: 'frontEnd/main/student/analyze/treatmentReport.html'},
         {name: 'Behavior Log', url: 'frontEnd/main/student/analyze/behaviorLog.html'},
         {name: 'Attendance Records', url: 'frontEnd/main/student/analyze/attendanceLog.html'}
     ];
-
-    // Temorarily removed: {name: 'Treatment Period', url: 'frontEnd/main/student/analyze/treatmentPeriod.html'},
 });
