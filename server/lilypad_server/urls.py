b5771db020259f9bd3a5ed5662ea6e39ab2d7e2c
Greg Nicholas <greg.nicholas@gmail.com>
2013-12-10 17:32:59 -0500

    begin simplification of API routes

--- server/lilypad_server/urls.py
+++ server/lilypad_server/urls.py
@@ -1,17 +1,30 @@
 from django.conf.urls import patterns, include, url
 
+from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-from pace import urls as pace_urls
+from pace import views as pace_views
 
+## set up URLs for API
+router = routers.SimpleRouter()
+router.register(r'classrooms', pace_views.ClassroomViewSet)
+router.register(r'students', pace_views.StudentViewSet)
+router.register(r'behaviortypes', pace_views.BehaviorTypeViewSet)
+
+apipatterns = router.urls
+# add non-router-based API paths
+apipatterns += patterns('',
+    url(r'^authtoken/$', 'rest_framework.authtoken.views.obtain_auth_token'),
+)
+apipatterns = format_suffix_patterns(apipatterns)
+
+## compile API and non-API url patterns together
 urlpatterns = patterns('',
     url(r'^$', 'lilypad_server.views.home'),
     url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^api/authtoken/$', 'rest_framework.authtoken.views.obtain_auth_token'),
-
-    # whole API is in pace app
-    url(r'^api/', include(pace_urls)),
+    url(r'^api/', include(apipatterns)),
 )
--- server/pace/serializers.py
+++ server/pace/serializers.py
@@ -1,108 +1,46 @@
 from rest_framework import serializers
-from common.serializers import NamespacedHyperlinkedModelSerializer, NamespacedHyperlinkedModelSerializerWithPKWrite
 
-from pace.models import Classroom, Student,             \
-                        PeriodicRecord, PointLoss,      \
+from pace.models import Classroom, Student,                        \
+                        PeriodicRecord, PointLoss,              \
                         BehaviorIncidentType, BehaviorIncident, \
                         Post, ReplyPost, AttendanceSpan
 
-class ClassroomSerializer(NamespacedHyperlinkedModelSerializer):
+class StudentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Student
+        fields = ('url', 'id', 'first_name', 'last_name',)
+
+class ClassroomSerializer(serializers.HyperlinkedModelSerializer):
+    students = StudentSerializer(many=True)
+
     class Meta:
         model = Classroom
-        fields = ('id', 'url', 'name',)
+        fields = ('id', 'url', 'name', 'students',)
 
-class AttendanceSpanSerializer(NamespacedHyperlinkedModelSerializer):
+class AttendanceSpanSerializer(serializers.HyperlinkedModelSerializer):
     student = serializers.PrimaryKeyRelatedField()
 
     class Meta:
         model = AttendanceSpan
-        fields = ('id', 'url', 'student', 'date', 'time_in', 'time_out')
+        fields = ('id', 'url', 'student', 'date', 'time_in', 'time_out',)
 
-class StudentSerializer(NamespacedHyperlinkedModelSerializer):
-    '''
-    Student serializer has a few customizations:
+def get_active_attendance_span(self, obj):
+    if self.attendance_anchor_dt:
+        date = self.attendance_anchor_dt.date()
+        time = self.attendance_anchor_dt.time()
 
-    1. Various URL fields to give shortcuts to student-specific data
-    2. A field called 'active_attendance_span' which will contain a serialized
-        AttendanceSpan object or None. This field is set by looking for an
-        AttendanceSpan for the student that is still open as of a set datetime
-        (specified in the constructor with the argument
-        `attendance_anchor_dt`).
+        spans = AttendanceSpan.objects.filter(
+            student=obj,
+            date=date,
+            time_in__lte=time,
+            time_out__isnull=True)
 
-    The code that adds this 'active_attendance_span' field is pretty hacky,
-    but it gets the job done. Might want to look into making it more robust
-    by playing nicer with the rest_framework API.
-    '''
-    periodic_records_url = serializers.HyperlinkedIdentityField(
-        view_name='pace:student_periodicrecord-list')
-    point_losses_url = serializers.HyperlinkedIdentityField(
-        view_name='pace:student_pointloss-list')
-    behavior_types_url = serializers.HyperlinkedIdentityField(
-        view_name='pace:student_behaviortype-list')
-    behavior_incidents_url = serializers.HyperlinkedIdentityField(
-        view_name='pace:student_behaviorincident-list')
-    posts_url = serializers.HyperlinkedIdentityField(
-        view_name='pace:student_post-list')
-    attendance_spans_url = serializers.HyperlinkedIdentityField(
-        view_name='pace:student_attendancespan-list')
+        if len(spans) > 0:
+            return spans[0]
 
-    class Meta:
-        model = Student
-        fields = ('url', 'id', 'first_name', 'last_name',
-            'periodic_records_url', 'point_losses_url',
-            'behavior_types_url', 'behavior_incidents_url',
-            'posts_url', 'attendance_spans_url')
+    return None
 
-    ### All of the code below is a quick and dirty way to add an
-    ### active_attendance_span field to this ModelSerializer. Definitely
-    ### a better way to do it than this, but just hacking into the
-    ### `data` object right before it's returned was the quickest way
-    ### to do it.
-
-    def __init__(self, *args, **kwargs):
-        self.attendance_anchor_dt = kwargs.pop('attendance_anchor_dt', None)
-        super(StudentSerializer, self).__init__(*args, **kwargs)
-
-    @property
-    def data(self):
-        # See notes above about this function
-        data = super(StudentSerializer, self).data
-        student = self.object
-
-        if not self.many:
-            data = [data]
-            student = [student]
-
-        for (dataset, obj) in zip(data, student):
-            active_span = self.get_active_attendance_span(obj)
-            if active_span:
-                # temporarily serializing whole span (card #130)
-                dataset['active_attendance_span'] = AttendanceSpanSerializer(active_span).data
-            else:
-                dataset['active_attendance_span'] = None
-
-        if self.many:
-            return data
-        else:
-            return data[0]
-
-    def get_active_attendance_span(self, obj):
-        if self.attendance_anchor_dt:
-            date = self.attendance_anchor_dt.date()
-            time = self.attendance_anchor_dt.time()
-
-            spans = AttendanceSpan.objects.filter(
-                student=obj,
-                date=date,
-                time_in__lte=time,
-                time_out__isnull=True)
-
-            if len(spans) > 0:
-                return spans[0]
-
-        return None
-
-class PointLossSerializer(NamespacedHyperlinkedModelSerializer):
+class PointLossSerializer(serializers.HyperlinkedModelSerializer):
     periodic_record = serializers.PrimaryKeyRelatedField()
 
     class Meta:
@@ -110,7 +48,7 @@ class PointLossSerializer(NamespacedHyperlinkedModelSerializer):
         fields = ('id', 'url', 'occurred_at', 'periodic_record',
             'point_type', 'comment')
 
-class PeriodicRecordSerializer(NamespacedHyperlinkedModelSerializer):
+class PeriodicRecordSerializer(serializers.HyperlinkedModelSerializer):
     student = serializers.PrimaryKeyRelatedField()
     point_losses = PointLossSerializer(many=True, required=False)
 
@@ -121,7 +59,7 @@ class PeriodicRecordSerializer(NamespacedHyperlinkedModelSerializer):
             'complete_work_points', 'follow_directions_points',
             'be_safe_points', 'point_losses')
 
-class BehaviorIncidentTypeSerializer(NamespacedHyperlinkedModelSerializer):
+class BehaviorIncidentTypeSerializer(serializers.HyperlinkedModelSerializer):
     applicable_student = serializers.PrimaryKeyRelatedField(required=False)
 
     class Meta:
@@ -129,7 +67,7 @@ class BehaviorIncidentTypeSerializer(NamespacedHyperlinkedModelSerializer):
         fields = ('id', 'url', 'label', 'code', 'supports_duration',
             'applicable_student')
 
-class BehaviorIncidentSerializer(NamespacedHyperlinkedModelSerializer):
+class BehaviorIncidentSerializer(serializers.HyperlinkedModelSerializer):
     student = serializers.PrimaryKeyRelatedField()
     type = serializers.PrimaryKeyRelatedField()
 
@@ -146,7 +84,7 @@ class ReplyPostSerializer(serializers.ModelSerializer):
         model = ReplyPost
         fields = ('author', 'content', 'created_at')
 
-class PostSerializer(NamespacedHyperlinkedModelSerializer):
+class PostSerializer(serializers.HyperlinkedModelSerializer):
     # TODO: make this a true User stub when user model worked out
     author = serializers.RelatedField()
     student = serializers.PrimaryKeyRelatedField()
