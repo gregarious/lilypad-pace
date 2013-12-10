9112667812dcf463ee6ba8cf94936652305cc62a
Greg Nicholas <greg.nicholas@gmail.com>
2013-12-10 14:34:05 -0500

    remove django-guardian package

--- server/lilypad_server/settings/base.py
+++ server/lilypad_server/settings/base.py
@@ -103,11 +103,6 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend', # default
-    'guardian.backends.ObjectPermissionBackend',
-)
-
 ROOT_URLCONF = 'lilypad_server.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
@@ -134,7 +129,6 @@ INSTALLED_APPS = (
     'django_extensions',
     'rest_framework',
     'rest_framework.authtoken',
-    'guardian',
 
     # our apps
     'pace',
@@ -148,9 +142,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     )
 }
-
-# necessary for django-guardian
-ANONYMOUS_USER_ID = -1
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
