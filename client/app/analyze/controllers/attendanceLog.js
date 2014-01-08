9aee3b83b2b34662ea7e15f5ff4a70a6ffce5a73
Greg Nicholas <greg.nicholas@gmail.com>
2014-01-08 00:44:35 -0500

    port attendance logs to use to API endpoint

--- client/app/analyze/controllers/attendanceLog.js
+++ client/app/analyze/controllers/attendanceLog.js
@@ -1,17 +1,28 @@
 // controller for the behavior log
-app.controller('AnalyzeAttendanceLogCtrl', function ($scope, attendanceDataStore) {
+app.controller('AnalyzeAttendanceLogCtrl', function ($scope, analyzeDataSources) {
 
+    $scope.attendanceSpanCollection = null;
+    $scope.statusMessage = '';
     $scope.$watch('viewState.selectedStudent', setAttendanceLogsForStudent);
 
     /**
      * Hooks $scope.attendanceSpanCollection up to the given student's data.
      */
     function setAttendanceLogsForStudent(student) {
+        $scope.attendanceSpanCollection = null;
         if (student) {
-            $scope.attendanceSpanCollection = attendanceDataStore.getForStudent(student);
+            $scope.statusMessage = "Fetching attendance logs...";
+            analyzeDataSources.fetchAttendanceLog(student).then(function(spans) {
+                $scope.attendanceSpanCollection = spans;
+                $scope.statusMessage = spans.length === 0 ?
+                    "No attendance records yet logged" :
+                    "";
+            }, function(response) {
+                $scope.statusMessage = "Error fetching logs";
+            });
         }
         else {
-            $scope.attendanceSpanCollection = null;
+            $scope.statusMessage = "No student selected";
         }
     }
 });
--- client/app/analyze/services/analyzeDataSources.js
+++ client/app/analyze/services/analyzeDataSources.js
@@ -1,4 +1,16 @@
-angular.module('pace').factory('analyzeDataSources', function($http, apiConfig) {
+angular.module('pace').factory('analyzeDataSources', function($http, $q, apiConfig, Backbone, AttendanceSpan) {
+    var AttendanceSpanCollection = Backbone.Collection.extend({
+        model: AttendanceSpan,
+        comparator: function(span) {
+            if (span.has('timeIn')) {
+                return -(moment(span.get('date') + 'T' + span.get('timeIn')).toDate());
+            }
+            else {
+                return -(moment(span.get('date')).toDate());
+            }
+        }
+    });
+
     return {
         /**
          * Returns a promise for an array of Attendance objects
@@ -9,7 +21,14 @@ angular.module('pace').factory('analyzeDataSources', function($http, apiConfig) 
          */
         fetchAttendanceLog: function(student) {
             var url = apiConfig.toAPIUrl('students/' + student.id + '/attendancespans/');
-            return $http.get(url);
+            var deferred = $q.defer();
+            $http.get(url).then(function(response) {
+                deferred.resolve(new AttendanceSpanCollection(response.data));
+            }, function(response) {
+                deferred.reject(response);
+            });
+
+            return deferred.promise;
         }
     };
 
