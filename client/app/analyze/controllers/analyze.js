9006fbda7e5ba992d26c37b79e756655f1fa8c10
Greg Nicholas <greg.nicholas@gmail.com>
2014-02-12 22:24:46 -0500

    add demo code for treatment periods

--- client/app/analyze/controllers/analyze.js
+++ client/app/analyze/controllers/analyze.js
@@ -2,8 +2,7 @@
 app.controller('StudentAnalyzeCtrl', function ($scope) {
     $scope.views = [
         {name: 'Rules', url: 'app/analyze/views/rules.html'},
-        // TODO: enable once this is ready
-        // {name: 'Treatment Report', url: 'app/analyze/views/treatmentReport.html'},
+        {name: 'Treatment Report', url: 'app/analyze/views/treatmentReport.html'},
         {name: 'Behavior Log', url: 'app/analyze/views/behaviorLog.html'},
         {name: 'Attendance Records', url: 'app/analyze/views/attendanceLog.html'}
     ];
