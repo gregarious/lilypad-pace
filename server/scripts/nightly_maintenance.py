cc7509a6a441eafc569cd3bb461b183530510a21
Greg Nicholas <greg.nicholas@gmail.com>
2014-04-01 08:46:14 -0400

    fixes to db backup script

--- server/scripts/nightly_maintenance.py
+++ server/scripts/nightly_maintenance.py
@@ -1,10 +1,15 @@
+import os
 from subprocess import call
 from django.utils import timezone
+from django.conf import settings
 
 def db_backup():
     filename = os.path.join(
-        os.path.dirname(PROJECT_ROOT),
+        os.path.dirname(settings.PROJECT_ROOT),
         'var',
         'lilypad_pace_%s.sql.gz' % timezone.now().date.strftime('%Y%m%d')
     )
     call('mysqldump -p lillypad_pace | gzip > %s' % filename)
+
+if __name__ == 'main':
+    db_backup()
