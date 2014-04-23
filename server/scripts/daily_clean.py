e0416481972fa1eac5c85e4e8e09fb09fd9a743a
Greg Nicholas <greg.nicholas@gmail.com>
2014-04-23 16:27:54 -0400

    add documentation, missing import

--- server/scripts/daily_clean.py
+++ server/scripts/daily_clean.py
@@ -1,5 +1,9 @@
+import datetime
 from pace.models import AttendanceSpan
 
-for span in AttendanceSpan.objects.filter(time_out__isnull=True):
+spans = AttendanceSpan.objects.filter(time_out__isnull=True)
+print 'Checking out %d students' % spans.count()
+
+for span in spans:
     span.time_out = datetime.time(15)
     span.save()
